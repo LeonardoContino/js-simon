8b2d86519baaf64d510e22da92976e7d5b126a07
LeonardoContino <leoconti2000@hotmail.it>
2022-12-20 16:33:48 +0100

    calcolato i numeri

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 // console.log("ok");
 
-const days = document.getElementById("days");
-const hours = document.getElementById("hours");
-const minutes = document.getElementById("minutes");
-const seconds = document.getElementById("seconds");
+let days = document.getElementById("days");
+let hours = document.getElementById("hours");
+let minutes = document.getElementById("minutes");
+let seconds = document.getElementById("seconds");
+
+//prendo l'anno corrente e la data del natale
+
+const xmasTime = new Date("December 25, 2022").getTime();
+
+console.log(xmasTime);
+
+const coundown = setInterval(() => {
+  const now = new Date().getTime;
+  const distance = xmasTime - now;
+
+  days.innerText = Math.floor(distance / (1000 * 60 * 60 * 24));
+  hours.innerText = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  minutes.innerText = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  seconds.innerText = Math.floor((distance % (1000 * 60)) / 1000);
+}, 1000);
